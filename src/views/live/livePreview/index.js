c825332e0a2fef72ba6968fc98f8e9f99bd6d36f
mengfanshuang <xiaoshuangogo@163.com>
2018-12-06 15:31:47 +0800

    直播预览接口更新

--- src/views/live/livePreview/index.js
+++ src/views/live/livePreview/index.js
@@ -2,42 +2,37 @@ import React, { Component } from 'react';
 import { Breadcrumb, Icon, Input, Pagination, message } from 'antd';
 import { Link } from 'react-router-dom';
 import './index.scss'
+import { withRouter } from 'react-router-dom'
 import ReactHLS from 'react-hls';
 import http from '../../../utils/http'
 
 const Search = Input.Search;
 
+@withRouter
 class LivePreview extends Component {
     state = {
-        currentTab: 1,
-        listData: [
-            {
-                status: 1,
-                cover: '',
-                url: '',
-                origin: 'PGM',
-                usePerson: '李雷',
-                pushUrl: '',
-                playUrl: ''
-            }
-        ]
+        currentTab: '',
+        listData: [],
+        projectInfo: null
     }
 
     componentDidMount () {
-        this.getInfos()
+        this.getInfos(1, 10, this.props.location.search.split('=')[1], '', '')
+        this.getProjectInfo()
     }
 
-    getInfos (current=1, size=10, projectName='', status='') {
+    getInfos (current=1, size=10, projectId='', status='', deviceName = '') {
         let params = {
             current: current,
             size: size,
-            projectName: projectName,
-            status: status
+            projectId: projectId,
+            streamingStatus: status,
+            deviceName: deviceName
         }
-        http.get('/api/projectInfo/list', params)
+        http.get('/api/projectInfo/streamList', params)
         .then(res => {
             if (res.code === 200) {
-                this.setState({listData: res.data.rows})
+                this.setState({listData: res.data})
             } else {
                 message.error(res.message)
             }
@@ -46,8 +41,22 @@ class LivePreview extends Component {
             message.error('网络连接失败，请稍后重试！')
         })
     }
+    getProjectInfo () {
+        http.get(`/api/projectInfo/detail/${this.props.location.search.split('=')[1]}`)
+        .then(res => {
+            if (res.code === 200) {
+                this.setState({projectInfo: res.data})
+            } else {
+                message.error(res.message)
+            }
+        })
+        .catch(error => {
+            message.error(`网络连接失败，请稍后重试！`)
+        })
+    }
     changeList = (type) => {            // 改变列表状态
         this.setState({currentTab: type})
+        this.getInfos(1, 10, this.props.location.search.split('=')[1], type, '')
     }
     render () {
         return (
@@ -59,44 +68,44 @@ class LivePreview extends Component {
                 </Breadcrumb>
                 <div className="live-preview-content">
                     <div className="live-preview-top clear">
-                        <h3>空军无人机回放列表</h3>
+                        <h3>{this.state.projectInfo && this.state.projectInfo.projectName}</h3>
                         <div className="right">
                             <div className="tabs-wrap">
                                 <span 
                                     style={{borderTopLeftRadius: 5,borderBottomLeftRadius: 5}} 
-                                    onClick={() => this.changeList(1)}
-                                    className={this.state.currentTab === 1 ? 
+                                    onClick={() => this.changeList('')}
+                                    className={this.state.currentTab === '' ? 
                                     'tabs-btn-item tabs-btn-choose' : 'tabs-btn-item'}>全部</span>
                                 <span 
                                     style={{borderRight: 0, borderLeft: 0}} 
-                                    onClick={() => this.changeList(2)}
-                                    className={this.state.currentTab === 2 ? 
+                                    onClick={() => this.changeList(1)}
+                                    className={this.state.currentTab === 1 ? 
                                     'tabs-btn-item tabs-btn-choose' : 'tabs-btn-item'}>推流中</span>
                                 <span 
                                     style={{borderTopRightRadius: 5, borderBottomRightRadius: 5}} 
-                                    onClick={() => this.changeList(3)}
-                                    className={this.state.currentTab === 3 ? 
+                                    onClick={() => this.changeList(0)}
+                                    className={this.state.currentTab === 0 ? 
                                     'tabs-btn-item tabs-btn-choose' : 'tabs-btn-item'}>未推流</span>
                             </div>
                             <Search
                                 placeholder="输入设备名"
-                                onSearch={value => console.log(value)}
+                                onSearch={value => this.getInfos(1, 10, this.props.location.search.split('=')[1], '', value)}
                                 style={{ width: 200 }}
                             />
                         </div>
                     </div>
                     <div className="push-address">
                         <span>直播推流地址：</span>
-                        rtmp://pili-publish.banmaim.com/banmaim-zhibo/live688-0-1534496255?key=86c26099c834c754
+                        {this.state.projectInfo && this.state.projectInfo.pushUrl}
                     </div>
                     <div className="push-address">
                         <span>RTMP播放地址：</span>
-                        rtmp://pili-publish.banmaim.com/banmaim-zhibo/live688-0-1534496255?key=86c26099c834c754
+                        {this.state.projectInfo && this.state.projectInfo.playUrl}
                     </div>
                     <ul className="live-preview-list">
                         {
                             this.state.listData.map((item, i) => (
-                                <li className="clear" key={item.id}>
+                                <li className="clear" key={item.name + item.pushUrl}>
                                     <div className="left-wrap">
                                         {
                                             !item.playUrl ?
@@ -113,12 +122,12 @@ class LivePreview extends Component {
                                     </div>
                                     <div className="right-wrap">
                                         <section className="list-item-detail">
-                                            <h3>{item.origin}</h3>
+                                            <h3>{item.name}</h3>
                                         </section>
                                         {
                                             item.crtUsrName !== '' &&
                                             <section className="list-item-detail">
-                                                <span className="created-person">使用者：{item.crtUsrName}</span>
+                                                <span className="created-person">使用者：{item.userName}</span>
                                             </section>
                                         }
                                         <div className="address-wrap">
